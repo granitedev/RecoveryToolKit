d115f9d87b869c1031abcb39b57a4a462b0e5bf9
devhero <michael.lewis@granitemountainbhc.com>
2017-10-01 19:05:19 -0700

    Updated Navigation

--- app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
+++ app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
@@ -19,12 +19,12 @@ public final class R {
         public static final int stackFromEnd = 0x7f010127;
     }
     public static final class dimen {
-        public static final int fastscroll_default_thickness = 0x7f080085;
-        public static final int fastscroll_margin = 0x7f080086;
-        public static final int fastscroll_minimum_range = 0x7f080087;
-        public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f08008f;
-        public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f080090;
-        public static final int item_touch_helper_swipe_escape_velocity = 0x7f080091;
+        public static final int fastscroll_default_thickness = 0x7f080087;
+        public static final int fastscroll_margin = 0x7f080088;
+        public static final int fastscroll_minimum_range = 0x7f080089;
+        public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f080091;
+        public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f080092;
+        public static final int item_touch_helper_swipe_escape_velocity = 0x7f080093;
     }
     public static final class id {
         public static final int item_touch_helper_previous_elevation = 0x7f0e0005;
--- app/src/main/java/com/granitemountainbhc/recoverytoolkit/MainActivity.java
+++ app/src/main/java/com/granitemountainbhc/recoverytoolkit/MainActivity.java
@@ -1,16 +1,16 @@
 package com.granitemountainbhc.recoverytoolkit;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
+import android.support.annotation.NonNull;
 import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -18,7 +18,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.Toast;
+
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,25 +29,34 @@ import java.util.List;
 /**
  * Provides UI for the main screen.
  */
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar mToolbar;
     private DrawerLayout mDrawerLayout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.MyMaterialTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Adding Toolbar to Main screen
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        setupToolbarMenu();
+        setupNavigationDrawerMenu();
+
+
+
         // Setting ViewPager for each Tabs
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
+
         // Create Navigation drawer and inlfate layout
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigationView);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         // Adding menu icon to Toolbar
+
         ActionBar supportActionBar = getSupportActionBar();
         if (supportActionBar != null) {
             VectorDrawableCompat indicator
@@ -60,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
                     // This method will trigger on item Click of navigation menu
                     @Override
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
+
                         // Set item in checked state
                         menuItem.setChecked(true);
 
@@ -71,6 +83,57 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+    }
+    private void setupToolbarMenu() {
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle("Navigation View");
+    }
+    private void setupNavigationDrawerMenu() {
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationView);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this,
+                mDrawerLayout,
+                mToolbar,
+                R.string.drawer_open,
+                R.string.drawer_close);
+
+        mDrawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+    }
+    @Override // Called when Any Navigation Item is Clicked
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+//		menuItem.setCheckable(true);
+//		menuItem.setChecked(true);  // This helps to know which Menu Item is Clicked
+
+        String itemName = (String) menuItem.getTitle();
+
+        Toast.makeText(MainActivity.this, itemName + " Clicked", Toast.LENGTH_SHORT).show();
+
+        closeDrawer();
+
+
+		return true;
+}
+    // Close the Drawer
+    private void closeDrawer() {
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+    }
+    // Open the Drawer
+    private void showDrawer() {
+        mDrawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START))
+            closeDrawer();
+        else
+            super.onBackPressed();
     }
 
     // Add Fragments to Tabs
